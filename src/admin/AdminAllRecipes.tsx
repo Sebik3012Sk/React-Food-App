474bb722fab00678aef66e460237a5c46adeb199
Nezo96 <racradoslav96@gmail.com>
2023-09-15 15:15:02 +0200

    Added functionality to AdminAllRecipes.tsx

--- src/admin/AdminAllRecipes.tsx
+++ src/admin/AdminAllRecipes.tsx
@@ -1,51 +1,49 @@
-import { FC } from "react";
-import { useState, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-interface DataApi {
-  recipe: [];
+interface PromiseData {
+  id: number;
+  image: string;
+  title: string;
+  recipe_text: string;
 }
 
 const AdminAllRecipes: FC = () => {
-  const [data_api, setDataApi] = useState([]);
+  const [apiData, setApiData] = useState<Array<PromiseData>>([]);
 
-  const getDataApi = async (): Promise<any> => {
+  const getDataApi = async (): Promise<Array<PromiseData> | any> => {
     const url = "http://localhost:8080/api-data";
     const response = await fetch(url);
     const data = await response.json();
-    setDataApi(data);
+    setApiData(data);
   };
 
   useEffect(() => {
     getDataApi();
   }, []);
 
-  console.log(data_api);
-
   return (
     <div>
       <div className="">
         <div className="flex flex-row flex-wrap items-center justify-center">
-          {data_api.map((recipe, index) => {
-            return (
-              <div
-                className="w-96 h-full p-5 rounded-xl m-10 bg-slate-300"
-                key={index}
-              >
-                <img
-                  className="w-72 h-64 rounded-xl"
-                  src={recipe.image}
-                  alt=""
-                />
-                <Link to={`/one-recipe/${recipe.id}`}>
-                  <span>
-                    <p className="text-2xl">{recipe.title}</p>
-                  </span>
-                </Link>
-                <p>{recipe.recipe_text}</p>
-              </div>
-            );
-          })}
+          {apiData.map((recipe, index) => (
+            <div
+              className="w-96 h-full p-5 rounded-xl m-10 bg-slate-300"
+              key={index}
+            >
+              <img
+                className="w-72 h-64 rounded-xl"
+                src={`http://localhost:8080/uploads/${recipe?.image}`}
+                alt={recipe?.title}
+              />
+              <Link to={`/one-recipe/${recipe?.id}`}>
+                <span>
+                  <p className="text-2xl">{recipe?.title}</p>
+                </span>
+              </Link>
+              <p>{recipe.recipe_text}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
